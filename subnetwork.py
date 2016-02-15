e4c4bcfbabc44f89773b8acfd5d6c41c68ea5073
pumpkinheader <pumpkinde@gmail.com>
2016-02-16 01:48:58 +0900

    user can configure the size of subnetwork

--- subnetwork.py
+++ subnetwork.py
@@ -13,14 +13,30 @@ def init_seeds(words):
     seeds.append(words)
     return seeds
     
-def sub_network(dic, start_words):
+def is_neighbor_needed(remained):
+    return remained > 0
+
+def no_neighbor_needed(remained):
+    return remained < 0
+    
+def sub_network(dic, start_words, distance):
     seeds = init_seeds(start_words)
     while not is_empty(seeds):
+        distance -= 1
+        next_seed = []
         words = seeds.pop(0)
+        if no_neighbor_needed(distance):
+            return "No edges were generated"
         for word in words:
             if dic.has_key(word):
                 nodes = dic[word]
                 add_all_edge(word, nodes)
+                next_seed.extend(node_words(nodes))
+        if is_neighbor_needed(distance):
+            seeds.append(next_seed)
+
+def node_words(nodes):
+    return nodes.keys()
                 
 def add_all_edge(start_word, nodes):
     for word in nodes.keys():
@@ -40,13 +56,16 @@ if __name__ == '__main__':
     print "start testing"
     dic = {}
     dic["A"] = {}
-    dic["A"]["C"] = 2
+    dic["A"]["B"] = 2
     dic["A"]["D"] = 1
     dic["B"] = {}
-    dic["B"]["A"] = 1
+    dic["B"]["C"] = 1
+    dic["B"]["E"] = 1
     print "####dic structure####"
     print dic
     print "#################"
-    sub_network(dic, ["A"])
+    print "input: distance"
+    N = input()
+    sub_network(dic, ["A"], N)
     err = draw_graph()
     print err
