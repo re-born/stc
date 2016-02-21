c938e00a4bae2986d5baa2d1f9aee58c0434a673
sosuke-k <snoopies.drum@gmail.com>
2016-02-21 14:08:02 +0900

    add index making script

---
+++ make_index.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from os import path
+import argparse
+import logging
+import time
+from progressbar import ProgressBar, Percentage, Bar
+
+from make_dic import noun_list
+import sqlconfig
+from sqltostc import read_table
+from index import Indexer
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('make_index')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file-path', type=str, default='./index.pkl')
+parser.add_argument('--overwrite', type=bool, default=False)
+args = parser.parse_args()
+
+
+def save_index(file_path):
+    query = "SELECT item_id, text FROM " + sqlconfig.tweet_table_name + " WHERE success = 1"
+    logger.info('query: {}'.format(query))
+
+    logger.info("SQL running...")
+    start = time.time()
+    rows = read_table(query)
+    elapsed_time = time.time() - start
+    logger.info("sql_time:{0}[sec]".format(elapsed_time))
+
+    logger.info("Indexing...")
+    start = time.time()
+    p = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(rows)).start()
+    indexer = Indexer()
+    for i, row in enumerate(rows):
+        indexer.add(row[0], noun_list(row[1]))
+        p.update(i + 1)
+    p.finish()
+    elapsed_time = time.time() - start
+    logger.info("indexing_time:{0}[sec]".format(elapsed_time))
+
+    logger.info("Saving...")
+    indexer.save(file_path)
+    logger.info('Done')
+
+
+if __name__ == "__main__":
+    if path.isfile(args.file_path):
+        logger.info('{} already exists'.format(args.file_path))
+        if args.overwrite:
+            logger.info('overwrite flag is True')
+            save_index(args.file_path)
+    else:
+        dirname = path.dirname(args.file_path)
+        if not path.isdir(dirname):
+            logger.info('create {}'.format(dirname))
+            os.mkdir(dirname)
+        save_index(args.file_path)
