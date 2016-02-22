dac7447f1cd81689a159ff6a966b1f52306194d0
pumpkinheader <pumpkinde@gmail.com>
2016-02-23 01:54:38 +0900

    /wc

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ print "load index....."
 indexer = Indexer()
 indexer.load("./index.pkl")
 print "Index loaded"
+print "word count / tweet dic....."
+with open('./word_count.pkl', 'r') as f:
+    wc_dic = cPickle.load(f)
+print "dic loaded"
+
 
 def retrieve_replies(input):
   text = input
@@ -27,6 +32,7 @@ def retrieve_replies(input):
   net.gen_sub_network(noun_list)
   queries = net.page_rank()
   
+  results = {}
   results = defaultdict(int)
   for query in queries:
     word = query
@@ -35,10 +41,13 @@ def retrieve_replies(input):
     for tup in tuple_list:
         results = indexer.update_replies(results, tup, score)
   results = tuples_from_dict(results)
-  return results
+  return normalize(results, wc_dic)
     
 def tuples_from_dict(dic):
     return sorted(dic.items(), key=lambda x:x[1], reverse=True)
+
+def normalize(results, dic):
+    return [ (tweet[0], tweet[1]/dic[tweet[0]]) for tweet in results]
 
 def test_data():
     return all_tweets(sqlconfig.run_table_name)
