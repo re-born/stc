6373906c5fce9603e02ff4ca02704a5f2ea9d423
sosuke-k <snoopies.drum@gmail.com>
2016-02-22 12:17:54 +0900

    move noun_list to stcutils and add Tweet class

---
+++ tests/test_utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Testing for Utils"""
+
+
+import os
+import sys
+import ipdb
+
+sys.path.append(os.getcwd())
+
+from unittest import TestCase
+import nose
+from nose.tools import eq_
+
+from stcutils import Tweet
+
+
+class UtilsTestCase(TestCase):
+
+    def setUp(self):
+        print ""
+
+    def test_tweet(self):
+        item_id = '554564419162619904'
+        t = Tweet(item_id)
+        eq_(t.item_id, item_id)
+        text = u'@take8823 三光堂さんにて、ポスター等置かせていただいたお礼を兼ねて、ご報告させていただきました。結構お持ちになられた方がいらしたようです。'
+        eq_(t.text, text)
+
+    def tearDown(self):
+        print "done"
