234d01112bb265d739d9c7f600b5a92744571cd7
sosuke-k <snoopies.drum@gmail.com>
2016-02-22 12:40:02 +0900

    add taskdata test code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,11 +20,19 @@ class UtilsTestCase(TestCase):
     def setUp(self):
         print ""
 
-    def test_tweet(self):
+    def test_testing_tweet(self):
         item_id = '554564419162619904'
         t = Tweet(item_id)
         eq_(t.item_id, item_id)
         text = u'@take8823 三光堂さんにて、ポスター等置かせていただいたお礼を兼ねて、ご報告させていただきました。結構お持ちになられた方がいらしたようです。'
+        eq_(t.text, text)
+
+    def test_taskdata_tweet(self):
+        item_id = '427930108028923905'
+        t = Tweet(item_id)
+        eq_(t.item_id, item_id)
+        # ipdb.set_trace()
+        text = u'@haramonera 遅れましたフォローサンクスです！'
         eq_(t.text, text)
 
     def tearDown(self):
