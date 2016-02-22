855ad0fd97ba949c53377a58cd56a1fe64d818c1
sosuke-k <snoopies.drum@gmail.com>
2016-02-23 00:17:57 +0900

    fix

--- sqlconfig.py
+++ sqlconfig.py
@@ -1,15 +1,8 @@
 # coding: utf-8
-# user = 'root'
-# host = 'localhost'
-# passwd = ''  # mysqlのrootユーザのパスワード
-# db = 'stc_test'  # 利用したいデータベースの名前
-# id_table_name = "stc_tweet_ids"
-# tweet_table_name = "stc_tweets"
-run_table_name = "test_tweets"
-
-user = 'trm'
+user = 'root'
 host = 'localhost'
-passwd = 'trm'  # mysqlのrootユーザのパスワード
-db = 'stc'  # 利用したいデータベースの名前
+passwd = ''  # mysqlのrootユーザのパスワード
+db = 'stc_test'  # 利用したいデータベースの名前
 id_table_name = "stc_tweet_ids"
 tweet_table_name = "stc_tweets"
+run_table_name = "test_tweets"
