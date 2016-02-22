099a4a57bf3c5f890915b146f843ab5630f2c298
kadotami <mondamin.kqfb@gmail.com>
2016-02-23 01:50:43 +0900

    edit wordcount

--- word_count.py
+++ word_count.py
@@ -15,12 +15,17 @@ import make_dic as md
 import sqlconfig
 import sqltostc
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--file-path', type=str, default='./word_count.pkl')
+parser.add_argument('--overwrite', type=bool, default=False)
+args = parser.parse_args()
+
 if __name__ == '__main__':
-    dic = defaultdict({lambda: 0})
+    dic = defaultdict(int)
     tweets = sqltostc.all_tweet_pairs()
     for i, tweet in enumerate(tweets):
         length = len(md.noun_list(tweet['R_TEXT']))
         dic[tweet['R_ID']] = length
-    with open('./word_count.pkl', 'wb') as f:
+    with open(args.file_path, 'wb') as f:
         cPickle.dump(dic, f, protocol=cPickle.HIGHEST_PROTOCOL)
 
